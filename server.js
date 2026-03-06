2e76a13c6d894fa45de351bc6ecbcbe41ffd3796
yashaswini-mn-26 <yashaswinimn26@gmail.com>
2026-03-06 13:15:32 +0530

    first commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://e-studyy.vercel.app/"
+    "https://e-studyy.vercel.app"
   ]
 }));
 app.use('/api/auth', authRoutes);
